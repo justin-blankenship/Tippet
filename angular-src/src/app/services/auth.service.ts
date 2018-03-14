5ca2e821c57e1b58ca95e3980cadc6f65c16597d
Justin Blankenship <jblanken@unca.edu>
2018-03-13 21:52:00 -0400

    Fixed authorization header errors for http get requests.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { HttpModule } from '@angular/http';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
@@ -25,11 +25,25 @@ export class AuthService {
   		.map(res => res.json());
   }
 
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', {headers: headers})
+      .map(res => res.json());
+  }
+
   storeUserData(token, user) {
   	localStorage.setItem('token', token);
   	localStorage.setItem('user', JSON.stringify(user));
   	this.authToken = token;
   	this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('token');
+    this.authToken = token;
   }
 
   logout() {
