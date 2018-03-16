3c4ea3f5ed66cd719fb3e8a1a38be210b73ced46
Justin Blankenship <jblanken@unca.edu>
2018-03-16 05:52:02 -0400

    Connect GH with Heroku.

---
+++ public/scripts.bundle.js
@@ -0,0 +1,8 @@
+//Force navbar to close when a route is selected
+$(document).on('click','.navbar-collapse.show',function(e) {
+	$(this).collapse('hide');
+});
+
+
+;
+//# sourceMappingURL=scripts.bundle.js.map
