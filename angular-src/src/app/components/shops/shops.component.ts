9c705b34c60c235722d71f33f7f56d61e0ba3e6d
Justin Blankenship <jblanken@unca.edu>
2018-04-04 03:13:36 -0400

    Added some fly shop data.

--- angular-src/src/app/components/shops/shops.component.ts
+++ angular-src/src/app/components/shops/shops.component.ts
@@ -22,11 +22,130 @@ export class ShopsComponent implements OnInit {
       czs:'Asheville, NC 28803'
     },
     {
-      name:'Hunter Banks Fly Fishing Co.',
+      name:'Hunter Banks Fly Fishing - Asheville',
       lat: 35.597075,
       lng: -82.559710,
       street:'29 Montford Ave',
       czs:'Asheville, NC 28801'
+    },
+    {
+      name:'Curtis Wright Outfitters & Fly Fishing Guides',
+      lat: 35.697227,
+      lng: -82.560670,
+      street:'24 N Main St',
+      czs:'Weaverville, NC 28787'
+    },
+    {
+      name:'One Fly Outfitters',
+      lat: 35.616641,
+      lng: -82.320943,
+      street:'112 Cherry St',
+      czs:'Black Mountain, NC 28711'
+    },
+    {
+      name:'Orvis',
+      lat: 35.485360,
+      lng: -82.554410,
+      street:'29 Schenck Pkwy #150',
+      czs:'Asheville, NC 28803'
+    },
+    {
+      name:'Brown Trout Fly Fishing LLC',
+      lat: 35.485216,
+      lng: -82.554342,
+      street:'28 Schenck Pkwy #150',
+      czs:'Asheville, NC 28803'
+    },
+    {
+      name:'Hunter Banks Fly Fishing - Waynesville',
+      lat: 35.490304,
+      lng: -82.987560,
+      street:'3889, 44 N Main St',
+      czs:'Waynesville, NC 28786'
+    },
+    {
+      name:'Davidson River Outfitters',
+      lat: 35.273898,
+      lng: -82.705076,
+      street:'49 Pisgah Hwy #6',
+      czs:'Pisgah Forest, NC 28768'
+    },
+    {
+      name:'Southern Drifters Outfitters',
+      lat: 35.917702,
+      lng: -82.299390,
+      street:'101 Town Square',
+      czs:'Burnsville, NC 28714'
+    },
+    {
+      name:'Tuckaseegee Fly Shop',
+      lat: 35.373695,
+      lng: -82.222569,
+      street:'534 W Main St',
+      czs:'Sylva, NC 28779'
+    },
+    {
+      name:'Highland Outfitters',
+      lat: 36.070849,
+      lng: -81.871577,
+      street:'4210 Mitchell Ave #1',
+      czs:'Linville, NC 28646'
+    },
+    {
+      name:'Hookers Fly Shop',
+      lat: 35.373795,
+      lng: -83.222812,
+      street:'546 W Main St',
+      czs:'Sylva, NC 28779'
+    },
+    {
+      name:'Tuckaseegee Fly Shop',
+      lat: 35.430864,
+      lng: -83.447172,
+      street:'3 Depot St',
+      czs:'Bryson City, NC 28713'
+    },
+    {
+      name:'Casters Fly Shop',
+      lat: 35.763500,
+      lng: -81.334150,
+      street:'2427 N Center St',
+      czs:'Hickory, NC 28601'
+    },
+    {
+      name:'Foscoe Fishing Company & Outfitters',
+      lat: 36.158438,
+      lng: -81.769415,
+      street:'8857 NC-105',
+      czs:'Boone, NC 28607'
+    },
+    {
+      name:'Appalachian Angler Fly Shop',
+      lat: 36.182085,
+      lng: -81.747424,
+      street:'174 Old Shulls Mill Rd',
+      czs:'Boone, NC 28607'
+    },
+    {
+      name:'Watauga River Fly Shop',
+      lat: 36.195083,
+      lng: -81.744858,
+      street:'5712 NC Highway 105 South',
+      czs:'Vilas, NC 28692'
+    },
+    {
+      name:'Due South Outfitters',
+      lat: 36.204993,
+      lng: -81.696859,
+      street:'2575 NC-105 Suite 60',
+      czs:'Boone, NC 28607'
+    },
+    {
+      name:'Madison River Fly Fishing Outfitters',
+      lat: 35.487777,
+      lng: -80.874737,
+      street:'20910 Torrence Chapel Rd',
+      czs:'Cornelius, NC 28031'
     }
   ];
 
--- public/main.bundle.js
+++ public/main.bundle.js
@@ -611,7 +611,7 @@ module.exports = "agm-map {\r\n\theight: 450px;\r\n}\r\n\r\n"
 /***/ "./src/app/components/shops/shops.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<!-- <div class=\"mt-3 col text-center mx-auto\">\n<h2 class=\"page-header\">Fly Shops Near You</h2>\n</div>\n<agm-map [latitude]=\"lat\" [longitude]=\"lng\" (mapClick)=\"selectedLocation($event)\">\n<agm-marker *ngIf=\"pinPlaced\" [latitude]=\"lat\" [longitude]=\"lng\"></agm-marker>\n</agm-map> -->\n\n<!-- img-fluid -->\n<div *ngIf=\"user\" class=\"container\">\n\n\t<div class=\"mt-20 col text-center mx-auto\">\n\t\t<h2 class=\"page-header\">Hello {{user.name}}.</h2>\n\t\t<h3>Use this map to find fly shops near you.</h3>\t\n\t</div>\n\n\t<div *ngIf=\"lat && lng\" class=\"mt-3\">\n\t\t<agm-map [latitude]=\"lat\" [longitude]=\"lng\">\n\t\t\t\n\t\t\t<agm-marker \n\t\t\t[latitude]=\"lat\" \n\t\t\t[longitude]=\"lng\">\n\t\t\t\t\n\t\t\t\t<agm-info-window>\n\t\t\t\t\t<strong>You Are Here</strong>\n\t\t\t\t</agm-info-window>\n\n\t\t\t</agm-marker>\n\n\t\t\t<agm-marker \n\t\t\t*ngFor=\"let p of pins\"\n\t\t\t[latitude]=\"p.lat\" \n\t\t\t[longitude]=\"p.lng\">\n\t\t\t\t\n\t\t\t\t<agm-info-window>\n\t\t\t\t\t<strong>{{p.name}}</strong>\n\t\t\t\t\t<span>{{p.street}}<br>{{p.czs}}</span>\n\t\t\t\t</agm-info-window>\n\t\t\t\t\n\t\t\t</agm-marker>\n\n\t\t</agm-map>\n\t</div>\n\n</div>"
+module.exports = "<!-- <div class=\"mt-3 col text-center mx-auto\">\n<h2 class=\"page-header\">Fly Shops Near You</h2>\n</div>\n<agm-map [latitude]=\"lat\" [longitude]=\"lng\" (mapClick)=\"selectedLocation($event)\">\n<agm-marker *ngIf=\"pinPlaced\" [latitude]=\"lat\" [longitude]=\"lng\"></agm-marker>\n</agm-map> -->\n\n<!-- img-fluid -->\n<div *ngIf=\"user\" class=\"container\">\n\n\t<div class=\"mt-20 col text-center mx-auto\">\n\t\t<h2 class=\"page-header\">Hello {{user.name}}.</h2>\n\t\t<h3>Use this map to find fly shops near you.</h3>\t\n\t</div>\n\n\t<div *ngIf=\"lat && lng\" class=\"mt-3\">\n\t\t<agm-map \n\t\t\t[latitude]=\"lat\" \n\t\t\t[longitude]=\"lng\"\n\t\t\t[zoom]=\"14\">\n\t\t\t\n\t\t\t<agm-marker \n\t\t\t[latitude]=\"lat\" \n\t\t\t[longitude]=\"lng\">\n\t\t\t\t\n\t\t\t\t<agm-info-window>\n\t\t\t\t\t<strong>You Are Here</strong>\n\t\t\t\t</agm-info-window>\n\n\t\t\t</agm-marker>\n\n\t\t\t<agm-marker \n\t\t\t*ngFor=\"let p of pins\"\n\t\t\t[latitude]=\"p.lat\" \n\t\t\t[longitude]=\"p.lng\">\n\t\t\t\t\n\t\t\t\t<agm-info-window>\n\t\t\t\t\t<strong>{{p.name}}</strong><br>\n\t\t\t\t\t<span>{{p.street}}<br>{{p.czs}}</span>\n\t\t\t\t</agm-info-window>\n\t\t\t\t\n\t\t\t</agm-marker>\n\n\t\t</agm-map>\n\t</div>\n\n</div>"
 
 /***/ }),
 
@@ -648,11 +648,130 @@ var ShopsComponent = /** @class */ (function () {
                 czs: 'Asheville, NC 28803'
             },
             {
-                name: 'Hunter Banks Fly Fishing Co.',
+                name: 'Hunter Banks Fly Fishing - Asheville',
                 lat: 35.597075,
                 lng: -82.559710,
                 street: '29 Montford Ave',
                 czs: 'Asheville, NC 28801'
+            },
+            {
+                name: 'Curtis Wright Outfitters & Fly Fishing Guides',
+                lat: 35.697227,
+                lng: -82.560670,
+                street: '24 N Main St',
+                czs: 'Weaverville, NC 28787'
+            },
+            {
+                name: 'One Fly Outfitters',
+                lat: 35.616641,
+                lng: -82.320943,
+                street: '112 Cherry St',
+                czs: 'Black Mountain, NC 28711'
+            },
+            {
+                name: 'Orvis',
+                lat: 35.485360,
+                lng: -82.554410,
+                street: '29 Schenck Pkwy #150',
+                czs: 'Asheville, NC 28803'
+            },
+            {
+                name: 'Brown Trout Fly Fishing LLC',
+                lat: 35.485216,
+                lng: -82.554342,
+                street: '28 Schenck Pkwy #150',
+                czs: 'Asheville, NC 28803'
+            },
+            {
+                name: 'Hunter Banks Fly Fishing - Waynesville',
+                lat: 35.490304,
+                lng: -82.987560,
+                street: '3889, 44 N Main St',
+                czs: 'Waynesville, NC 28786'
+            },
+            {
+                name: 'Davidson River Outfitters',
+                lat: 35.273898,
+                lng: -82.705076,
+                street: '49 Pisgah Hwy #6',
+                czs: 'Pisgah Forest, NC 28768'
+            },
+            {
+                name: 'Southern Drifters Outfitters',
+                lat: 35.917702,
+                lng: -82.299390,
+                street: '101 Town Square',
+                czs: 'Burnsville, NC 28714'
+            },
+            {
+                name: 'Tuckaseegee Fly Shop',
+                lat: 35.373695,
+                lng: -82.222569,
+                street: '534 W Main St',
+                czs: 'Sylva, NC 28779'
+            },
+            {
+                name: 'Highland Outfitters',
+                lat: 36.070849,
+                lng: -81.871577,
+                street: '4210 Mitchell Ave #1',
+                czs: 'Linville, NC 28646'
+            },
+            {
+                name: 'Hookers Fly Shop',
+                lat: 35.373795,
+                lng: -83.222812,
+                street: '546 W Main St',
+                czs: 'Sylva, NC 28779'
+            },
+            {
+                name: 'Tuckaseegee Fly Shop',
+                lat: 35.430864,
+                lng: -83.447172,
+                street: '3 Depot St',
+                czs: 'Bryson City, NC 28713'
+            },
+            {
+                name: 'Casters Fly Shop',
+                lat: 35.763500,
+                lng: -81.334150,
+                street: '2427 N Center St',
+                czs: 'Hickory, NC 28601'
+            },
+            {
+                name: 'Foscoe Fishing Company & Outfitters',
+                lat: 36.158438,
+                lng: -81.769415,
+                street: '8857 NC-105',
+                czs: 'Boone, NC 28607'
+            },
+            {
+                name: 'Appalachian Angler Fly Shop',
+                lat: 36.182085,
+                lng: -81.747424,
+                street: '174 Old Shulls Mill Rd',
+                czs: 'Boone, NC 28607'
+            },
+            {
+                name: 'Watauga River Fly Shop',
+                lat: 36.195083,
+                lng: -81.744858,
+                street: '5712 NC Highway 105 South',
+                czs: 'Vilas, NC 28692'
+            },
+            {
+                name: 'Due South Outfitters',
+                lat: 36.204993,
+                lng: -81.696859,
+                street: '2575 NC-105 Suite 60',
+                czs: 'Boone, NC 28607'
+            },
+            {
+                name: 'Madison River Fly Fishing Outfitters',
+                lat: 35.487777,
+                lng: -80.874737,
+                street: '20910 Torrence Chapel Rd',
+                czs: 'Cornelius, NC 28031'
             }
         ];
     }
