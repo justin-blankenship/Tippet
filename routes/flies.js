f7289f0256d7c1e7f418bf5ba36b26c90ac5a64f
Justin Blankenship <jblanken@unca.edu>
2018-04-17 23:19:17 -0400

    Changed fly route.

--- routes/flies.js
+++ routes/flies.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const config = require('../config/database');
 const Fly = require('../models/fly');
 
-//Get all flies
-router.get('/', function(req, res, next) {
-	Fly.find(function(err, fly) {
-		if(err) return next(err);
-		return res.json(fly);
-	});
+// //Get all flies
+// router.get('/', function(req, res, next) {
+// 	Fly.find(function(err, fly) {
+// 		if(err) return next(err);
+// 		return res.json(fly);
+// 	});
+// });
+
+
+//Profile
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+	res.json({user:req.user});
 });
 
 module.exports = router;
