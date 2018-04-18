1d84781bc491b8bc4bf08020e9f049a996d5b310
Justin Blankenship <jblanken@unca.edu>
2018-04-17 21:01:44 -0400

    Modified flies route script.

--- routes/flies.js
+++ routes/flies.js
@@ -4,11 +4,11 @@ const config = require('../config/database');
 const Fly = require('../models/fly');
 
 //Get all flies
-router.get('/', function(req, res, next) {
-	Fly.find(function(err, fly) {
-		if(err) return next(err);
-		return res.json(fly);
-	});
-});
+// router.get('/', function(req, res, next) {
+// 	Fly.find(function(err, fly) {
+// 		if(err) return next(err);
+// 		return res.json(fly);
+// 	});
+// });
 
 module.exports = router;
