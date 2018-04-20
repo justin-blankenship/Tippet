611e7fbd716e9b7ca64ebf8e4ecda8c799d01b79
Justin Blankenship <jblanken@unca.edu>
2018-04-20 15:32:18 -0400

    Modify profile page.

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-profile',
@@ -10,13 +12,18 @@ import { Router } from '@angular/router';
 
 export class ProfileComponent implements OnInit {
 	user: Object;
+  fly:Object;
 
   constructor(
   	public authService: AuthService, 
-  	private router: Router
+  	private router: Router,
+    private http: HttpClient
   	) { }
 
   ngOnInit() {
+    this.http.get('/flies').subscribe(data => {
+      this.fly = data;
+    });
   	this.authService.getProfile().subscribe(profile => {
   		this.user = profile.user;
   	},
