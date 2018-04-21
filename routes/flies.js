62a86cfe20700003d9f254566a3d9c28c6422c93
Justin Blankenship <jblanken@unca.edu>
2018-04-20 21:04:36 -0400

    Omit fly route.

--- routes/flies.js
+++ routes/flies.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const config = require('../config/database');
-const Fly = require('../models/fly');
+// const express = require('express');
+// const router = express.Router();
+// const config = require('../config/database');
+// const Fly = require('../models/fly');
 
-//Get all flies
-router.get('/', function(req, res, next) {
-	Fly.find(function(err, fly) {
-		if(err) return next(err);
-		return res.json(fly);
-	});
-});
+// //Get all flies
+// router.get('/', function(req, res, next) {
+// 	Fly.find(function(err, fly) {
+// 		if(err) return next(err);
+// 		return res.json(fly);
+// 	});
+// });
 
-module.exports = router;
+// module.exports = router;
